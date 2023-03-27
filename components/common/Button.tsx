0a190c90df9cf7e261b23e11146bf0717fd28c79
Ycow <skypnal12@gmail.com>
2023-03-27 23:56:37 +0900

    feat: Button 컴포넌트 생성

---
+++ components/common/Button.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: block;
+  outline: none;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  height: 2.25rem;
+  font-size: 1rem;
+
+  background: #228be6;
+  &:hover {
+    background: #339af0;
+  }
+  &:active {
+    background: #1c7ed6;
+  }
+`;
+
+export default Button;
