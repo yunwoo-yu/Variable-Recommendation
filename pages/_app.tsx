fdb68af72cf149e190dc25a5c42388554c0e4bd0
Ycow <skypnal12@gmail.com>
2023-03-28 00:15:41 +0900

    feat: toastify 알림창 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import GlobalStyle from '@/styles/Globalstyle';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import '../styles/index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import FeaturedList from '@/components/FeaturedList/FeaturedList';
 import QuestionForm from '@/components/QuestionForm/QuestionForm';
 import Head from 'next/head';
 import { useState } from 'react';
+import { ToastContainer } from 'react-toastify';
 
 export default function Home() {
   const [result, setResult] = useState<string[]>([]);
@@ -9,12 +10,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta title="" />
+        <title>변수명을 부탁해</title>
       </Head>
       <main>
+        <h1>변수명을 부탁해</h1>
         <QuestionForm />
         <FeaturedList />
       </main>
+      <ToastContainer
+        position="top-center"
+        theme="light"
+        autoClose={1500}
+        pauseOnFocusLoss={false}
+        limit={1}
+      />
     </>
   );
 }
