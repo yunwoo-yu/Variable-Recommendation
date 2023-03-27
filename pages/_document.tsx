373e11c13cb4b35bb72279f5d5087c9f6539bd3c
Ycow <skypnal12@gmail.com>
2023-03-28 00:15:29 +0900

    feat: styled 플릭현상 제거

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,42 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      const styleElement = [
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>,
+      ];
+      return {
+        ...initialProps,
+        styles: styleElement,
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
