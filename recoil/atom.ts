97dbcf13b554df6f3469b65d05c0f3d8c545836e
Ycow <skypnal12@gmail.com>
2023-03-28 21:19:17 +0900

    feat: atom id 타입 추가 질문 state 전역으로 변경

--- recoil/atom.ts
+++ recoil/atom.ts
@@ -2,11 +2,18 @@ import { atom } from 'recoil';
 
 interface RecommendedVariableResult {
   result: string[];
+  id: string;
 }
 
 export const recommendedVariableResultAtom = atom<RecommendedVariableResult>({
   key: 'recommendedVariable',
   default: {
     result: [],
+    id: '',
   },
+});
+
+export const questionValueAtom = atom<string>({
+  key: 'question',
+  default: '',
 });
