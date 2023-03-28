4ea09db6d12761718f8a0283cf1ecea9aac23085
Ycow <skypnal12@gmail.com>
2023-03-28 21:17:45 +0900

    feat: 쿼리 프로바이더 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,17 @@ import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import '../styles/index.css';
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
