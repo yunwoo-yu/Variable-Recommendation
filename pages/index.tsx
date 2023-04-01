4fffb0719b1499361b596ba6332e3d13fbd1c4bc
Ycow <skypnal12@gmail.com>
2023-04-02 04:29:32 +0900

    feat: 컨테이너 추가, 모달 버튼 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,45 @@
-import FeaturedList from '@/components/FeaturedList/FeaturedList';
-import Footer from '@/components/Footer/Footer';
 import QuestionForm from '@/components/QuestionForm/QuestionForm';
 import Head from 'next/head';
-import { ToastContainer } from 'react-toastify';
+import useModal from '@/hooks/useModal';
+import Button from '@/components/common/Button';
+import styled from 'styled-components';
+import Image from 'next/image';
+import WarningIcon from '../lib/assets/warning-icon.svg';
 
 export default function Home() {
+  const { onClickIsToggleModal } = useModal();
+
   return (
-    <>
+    <Container>
       <Head>
         <title>변수명을 부탁해</title>
       </Head>
+      <Button onClick={onClickIsToggleModal}>
+        <Image src={WarningIcon} alt="필독사항 버튼" />
+        사용 전 읽어주세요
+      </Button>
       <QuestionForm />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.section`
+  margin: 42px 0;
+
+  > button {
+    display: flex;
+    margin-left: auto;
+    margin-bottom: 24px;
+    align-items: center;
+    line-height: 24px;
+    padding: 4px 16px;
+    border-radius: 4px;
+
+    > img {
+      margin-right: 8px;
+      fill: #ffffff;
+      filter: invert(100%) sepia(35%) saturate(1180%) hue-rotate(186deg) brightness(107%)
+        contrast(107%);
+    }
+  }
+`;
