17d30c5696dc4dc7e6be877d47dadf4cb25632fc
Ycow <skypnal12@gmail.com>
2023-04-03 02:43:53 +0900

    feat: 메타데이터 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,42 @@ import '../lib/styles/index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Layout from '@/components/common/Layout';
+import Head from 'next/head';
+
+const metaData = {
+  title: '변수명을 부탁해 - 변수명 추천 사이트',
+  description: '입력받은 텍스트의 변수명을 추천해주는 사이트입니다.',
+  image: '/images/thumbnail.png',
+  width: '1200',
+  height: '630',
+};
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Layout>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://variable-recommendation.vercel.app/" />
+        <meta property="og:title" content={metaData.title} />
+        <meta property="og:description" content={metaData.description} />
+        <meta property="og:image" content={metaData.image} />
+        <meta property="og:image:alt" content={metaData.title} />
+        <meta property="og:image:width" content={metaData.width} />
+        <meta property="og:image:height" content={metaData.height} />
+        <meta property="og:site_name" content={metaData.title} />
+        <meta property="og:locale" content="ko_KR" />
+        <title>변수명을 부탁해</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Layout>
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </Layout>
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
   );
 }
